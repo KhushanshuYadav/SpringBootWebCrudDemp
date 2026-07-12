71446d34d73f333310738fcca4703766aad13826
Khushanshu Yadav <khushanshuyadav2@gmail.com>
2026-07-13 00:13:22 +0530

    Added Filters

---
+++ CrudDemoBoot/src/main/java/com/khushanshu/CrudDemoBoot/filters/AuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.khushanshu.CrudDemoBoot.filters;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@WebFilter("/api")
+@Order(1)
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest=(HttpServletRequest) request;
+        HttpServletResponse httpServletResponse=(HttpServletResponse) response;
+
+        String code =httpServletRequest.getHeader("code");
+
+        if(code==null || !code.equals("123")) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        chain.doFilter(request,response);
+
+    }
+}
